6ab3336eec4ee7c1ced6bf3e8ff6d7f89696ed7e
Omar Hussam <omarhussamohamed@gmail.com>
2026-01-25 01:29:07 +0200

    fix: Correct encoding declaration position in KVSStreamerNode script

--- scripts/kvs_streamer_node.py
+++ scripts/kvs_streamer_node.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 #!/usr/bin/env python2
+# -*- coding: utf-8 -*-
 """
 AWS Kinesis Video Streams Integration Node - OPTIMIZED
 Streams 1280x720 @ 30fps camera feed to AWS KVS
